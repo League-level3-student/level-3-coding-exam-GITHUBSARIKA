08f4bebd517d033de674ac262b64eba1c52404bc
League Student <league@iMac-22.attlocal.net>
2019-10-10 18:27:39 -0700

    coding exam 2

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
+
+	public Object calculateElectionWinner(ArrayList<String> votes) {
+		String TIE;
+		int votesFrancis=0;
+		     int votesSnowden=0;
+		for (int i = 0; i < votes.size(); i++) {
+			votes.get(i);			
+		     
+			if (votes.get(i).equalsIgnoreCase("pope francis")) {
+				votesFrancis++;
+				
+			}
+			if (votes.get(i).equalsIgnoreCase("edward snowden")) {
+				votesSnowden++;
+				
+			}
+			
+		
+	}
+	if(votesFrancis==votesSnowden) {
+				return "TIE";
+			}
+	if(votesFrancis>votesSnowden) {
+		return "pope francis";
+	}
+	if(votesSnowden>votesFrancis) {
+		return "edward snowden";
+	}
+	return null;
+		}
 
 }
---
+++ test/HashMapCalculator.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class HashMapCalculator {
+
+	public Object commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
+		// TODO Auto-generated method stub
+		int amountOfMatchingEntries=0;
+		for (int i = 0; i < hashmap1.size(); i++) {
+			hashmap1.get(i);
+		}
+		for (int i = 0; i < hashmap2.size(); i++) {
+			hashmap2.get(i);
+		}
+		return null;
+	}
+
+}
